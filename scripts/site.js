b468e8bd177a443d166474d3abdef00a17578e55
Megan Byers | Design Elixir <megan@designelixir.studio>
2023-04-21 03:12:55 -0600

    home region build

--- scripts/site.js
+++ scripts/site.js
@@ -24,3 +24,9 @@
   window.addEventListener('resize', loadAllImages);
 
 }());
+
+function goToAnchor(anchor) {
+  var loc = document.location.toString().split('#')[0];
+  document.location = loc + '#' + anchor;
+  return false;
+}
